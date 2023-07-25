27b66185400baa2fba9b250b481a2186e72d4e59
lhw <lihaiwei19921122@163.com>
2023-07-25 21:01:44 +0800

    more Component

--- src/main.rs
+++ src/main.rs
@@ -33,9 +33,38 @@ impl MyEguiApp {
 
 impl eframe::App for MyEguiApp {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-           ui.label("This is a label");
-       });
-   }
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+            ui.label("This is a label");
+            ui.label("This is a label");
+            ui.hyperlink("https://github.com/emilk/egui");
+            let mut my_string = String::new();
+            ui.text_edit_singleline(&mut my_string);
+            if ui.button("Click me").clicked() { }
+            let mut my_f32 = 0.0;
+            ui.add(egui::Slider::new(&mut my_f32, 0.0..=100.0));
+            ui.add(egui::DragValue::new(&mut my_f32));
+
+            let mut my_boolean = false;
+            ui.checkbox(&mut my_boolean, "Checkbox");
+
+            #[derive(PartialEq)]
+            enum Enum { First, Second, Third }
+            let mut my_enum = Enum::First;
+            ui.horizontal(|ui| {
+                ui.radio_value(&mut my_enum, Enum::First, "First");
+                ui.radio_value(&mut my_enum, Enum::Second, "Second");
+                ui.radio_value(&mut my_enum, Enum::Third, "Third");
+            });
+
+            ui.separator();
+
+            // let my_image = "assets/graphic/city.png";
+            // ui.image(my_image, [640.0, 480.0]);
+
+            ui.collapsing("Click to see what is hidden!", |ui| {
+                ui.label("Not much, as it turns out");
+            });
+        });
+    }
 }
