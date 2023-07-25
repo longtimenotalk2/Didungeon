59488b4319ec32b085e1979305e4cc6479b61590
lhw <lihaiwei19921122@163.com>
2023-07-25 20:54:22 +0800

    draw a label

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ impl eframe::App for MyEguiApp {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
        egui::CentralPanel::default().show(ctx, |ui| {
            ui.heading("Hello World!");
+           ui.label("This is a label");
        });
    }
 }
